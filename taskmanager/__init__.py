deb506ea6c46c7a1fee19963c5a907fdcb02f005
ajorgensen89 <ajorgensen89@gmail.com>
2023-05-12 10:16:05 +0000

    Trial add category

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,18 +1,14 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-# To use hidden file path.
 if os.path.exists("env.py"):
     import env  # noqa
 
-# Create an instance for Flask - app.
+
 app = Flask(__name__)
-# Set to environment variable.
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
-# Create an instance for SQLAlchemy
 db = SQLAlchemy(app)
 
-# Defined after the routed variables above.
 from taskmanager import routes  # noqa
--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,9 +1,6 @@
-from flask import render_template
-# app, db variable defined in __init__.py file.
+from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
 from taskmanager.models import Category, Task
-
-# Create app route using '/'
 
 
 @app.route("/")
@@ -13,9 +10,15 @@ def home():
 
 @app.route("/categories")
 def categories():
-    return render_template("categories.html")
+    categories = list(Category.query.order_by(Category.category_name).all())
+    return render_template("categories.html", categories=categories)
 
 
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
     return render_template("add_category.html")
